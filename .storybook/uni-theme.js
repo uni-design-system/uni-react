615bd6d18dfdf081424dc1d381f95bbee7f4bc0a
George English <gaenglish@gmail.com>
2022-03-17 14:01:40 -0400

    updating docs

--- .storybook/uni-theme.js
+++ .storybook/uni-theme.js
@@ -4,7 +4,7 @@ import { default as brandImage } from './uni-storybook-logo.png'
 export default create({
   base: 'light',
   brandTitle: 'UNI Design System',
-  brandUrl: 'https://github.com/uni-design-system',
+  brandUrl: 'https://uni-design-system.github.io/uni-react/',
   brandImage,
 
   appBg: 'white'
