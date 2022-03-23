b181c2a2f19a3ab0c61cc3ceef87d7617c8f5e03
George English <gaenglish@gmail.com>
2022-03-22 21:12:16 -0400

    updating stories

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -6,13 +6,39 @@ export default {
   component: Button
 };
 
-export const BasicButton = (args: ButtonProps) => <Button {...args} />;
-BasicButton.args = {
+export const Filled = (args: ButtonProps) => <Button {...args} />;
+
+Filled.args = {
   disabled: false,
   buttonType: 'filled',
-  children: 'Sample'
+  children: 'Filled Button'
 }
 
-BasicButton.story = {
-  name: 'Button',
+Filled.story = {
+  name: 'Filled Button',
+};
+
+
+export const Elevated = (args: ButtonProps) => <Button {...args} />;
+
+Elevated.args = {
+  disabled: false,
+  buttonType: 'elevated',
+  children: 'Elevated Button'
+}
+
+Elevated.story = {
+  name: 'Elevated Button',
+};
+
+export const Outlined = (args: ButtonProps) => <Button {...args} />;
+
+Outlined.args = {
+  disabled: false,
+  buttonType: 'outlined',
+  children: 'Outlined Button'
+}
+
+Outlined.story = {
+  name: 'Outlined Button',
 };
