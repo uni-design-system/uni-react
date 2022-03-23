2e518fd282762341f715e934bcf2e45b6612b2ee
George English <gaenglish@gmail.com>
2022-03-22 20:29:57 -0400

    adding basic container

---
+++ src/core/container/container.component.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from 'react';
+
+export interface ContainerProps {
+  children: ReactNode;
+
+}
+
+export default function Container({ children }: ContainerProps) {
+
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
