e1a0c4823499f6f1804be7ae1a81714636da6b5e
George English <gaenglish@gmail.com>
2022-12-22 15:31:18 -0500

    configuring assets

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,11 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@react-theming/storybook-addon',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@react-theming/storybook-addon"
-  ],
-  "framework": "@storybook/react"
-}
+  framework: '@storybook/react',
+  staticDirs: ['./.storybook/favicon', '../assets'],
+};
