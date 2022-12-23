a1f1364593121636e1065e5f1f4789dc60b12bda
George English <gaenglish@gmail.com>
2022-12-23 09:19:40 -0500

    updating core

--- src/core/overlay/overlay.config.ts
+++ src/core/overlay/overlay.config.ts
@@ -1,4 +1,4 @@
-import { Theme, hslaToString } from '@uni-design-system/uni-core';
+import { Theme, HSLAToString } from '@uni-design-system/uni-core';
 import { CSSProperties } from 'react';
 import { OverlayProps } from './overlay.model';
 
@@ -6,7 +6,7 @@ export const OverlayStyle = (
   theme: Theme,
   { hueRotateDeg = 0, hueDeg = 0, saturation = 0, lightness = 0, transparency = 0.5, blurPx = 0 }: OverlayProps,
 ): CSSProperties => {
-  const backgroundColor = hslaToString({ hue: hueDeg, saturation, lightness, alpha: transparency });
+  const backgroundColor = HSLAToString({ hue: hueDeg, saturation, lightness, alpha: transparency });
 
   return {
     position: 'fixed',
