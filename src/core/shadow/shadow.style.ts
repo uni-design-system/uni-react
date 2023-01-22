806b459086f80d39c70f4c7105d7f1f8db90a2aa
George English <gaenglish@gmail.com>
2023-01-22 14:13:29 -0500

    updating shadows

--- src/core/shadow/shadow.style.ts
+++ src/core/shadow/shadow.style.ts
@@ -1,10 +1,5 @@
-import { ShadowElevation, ShadowMap } from '@uni-design-system/uni-core';
+import { ShadowElevation, ShadowCssMap } from '@uni-design-system/uni-core';
 
 export function BoxShadow(elevation: ShadowElevation) {
-  const shadowDef = ShadowMap[elevation];
-
-  const umbra = `0 ${shadowDef.umbra.offset}px ${shadowDef.umbra.blur}px rgba(0,0,0,0.${shadowDef.umbra.opacity})`;
-  const penumbra = `0 ${shadowDef.penumbra.offset}px ${shadowDef.penumbra.blur}px rgba(0,0,0,0.${shadowDef.penumbra.opacity})`;
-
-  return [umbra, penumbra].join(', ');
+  return ShadowCssMap[elevation];
 }
