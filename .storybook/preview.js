2632f9b995837c9d21b4c5f303ac5b4a40376129
George English <gaenglish@gmail.com>
2023-02-06 12:30:34 -0500

    adding square theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,11 @@
-import {ThemeProvider} from "../src/core/theme/theme.provider";
-import {BuiltInThemes} from "@uni-design-system/uni-core";
-import {addDecorator} from "@storybook/react";
-import React from "react";
-import {withThemes} from "@react-theming/storybook-addon/dist/preview";
-import {LayoutProvider} from "../src/core/layout/layout.provider";
+import { ThemeProvider } from '../src/core/theme/theme.provider';
+import { BuiltInThemes } from '@uni-design-system/uni-core';
+import { addDecorator } from '@storybook/react';
+import React from 'react';
+import { withThemes } from '@react-theming/storybook-addon/dist/preview';
+import { LayoutProvider } from '../src/core/layout/layout.provider';
 
-const providerFn = ({theme, children}) => {
+const providerFn = ({ theme, children }) => {
   return (
     <LayoutProvider>
       <ThemeProvider themeId={theme.id} themes={BuiltInThemes}>
@@ -15,15 +15,16 @@ const providerFn = ({theme, children}) => {
   );
 };
 
-addDecorator(withThemes(null, [BuiltInThemes.LightTheme, BuiltInThemes.DarkTheme], { providerFn }));
+const Themes = [BuiltInThemes.LightTheme, BuiltInThemes.DarkTheme, BuiltInThemes.SquareTheme];
 
+addDecorator(withThemes(null, Themes, { providerFn }));
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
