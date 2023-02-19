aafe12c6b649c53376332679b7fa41e904507135
George English <gaenglish@gmail.com>
2023-02-19 14:30:48 -0500

    adding open modal button

--- src/components/modal/modal.stories.tsx
+++ src/components/modal/modal.stories.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal, ModalProps } from './modal.component';
 import { Text } from '../../core';
+import { Button } from '../button';
 
 export default {
   title: 'Components/Modal',
   component: Modal,
 };
 
-export const Basic = (args: ModalProps) => (
-  <Modal {...args}>
-    <Text role="body-1-short">This is a simple modal component.</Text>
-  </Modal>
-);
+export const Basic = (args: ModalProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setIsOpen(true)}>Open Modal</Button>
+
+      <Modal {...args} isOpen={isOpen}>
+        <Button buttonType="icon" iconName="xmarkSolid" onClick={() => setIsOpen(false)} />
+        <Text role="body-1-short">This is a simple modal component.</Text>
+      </Modal>
+    </>
+  );
+};
