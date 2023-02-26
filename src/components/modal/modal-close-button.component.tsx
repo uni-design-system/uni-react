fd046f5911aaec777bcbf4d0058778f84fef9fc4
George English <gaenglish@gmail.com>
2023-02-26 15:37:22 -0500

    adding modal close button

---
+++ src/components/modal/modal-close-button.component.tsx
@@ -0,0 +1,12 @@
+import React, { CSSProperties, PropsWithChildren } from 'react';
+import { Button, ButtonProps } from '../button';
+
+export const ModalCloseButton = ({ ...rest }: PropsWithChildren<ButtonProps>): JSX.Element => {
+  const style: CSSProperties = {
+    position: 'absolute',
+    top: '8px',
+    right: '8px',
+  };
+
+  return <Button useRipple={false} buttonType="icon" iconName="xmarkSolid" style={style} {...rest} />;
+};
--- src/components/modal/modal.stories.tsx
+++ src/components/modal/modal.stories.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Modal, ModalProps } from './modal.component';
 import { Text } from '../../core';
 import { Button } from '../button';
+import { ModalCloseButton } from './modal-close-button.component';
 
 export default {
   title: 'Components/Modal',
@@ -16,7 +17,7 @@ export const Basic = (args: ModalProps) => {
       <Button onClick={() => setIsOpen(true)}>Open Modal</Button>
 
       <Modal {...args} isOpen={isOpen}>
-        <Button buttonType="icon" iconName="xmarkSolid" onClick={() => setIsOpen(false)} />
+        <ModalCloseButton onClick={() => setIsOpen(false)} />
         <Text role="body-1-short">This is a simple modal component.</Text>
       </Modal>
     </>
