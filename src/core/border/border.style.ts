a626a15230027fad0b9801a3d61a21dd4f840574
George English <gaenglish@gmail.com>
2023-03-02 12:41:19 -0500

    updating defaults

--- src/core/border/border.style.ts
+++ src/core/border/border.style.ts
@@ -2,17 +2,16 @@ import { Border, ColorToken, getValue, Theme } from '@uni-design-system/uni-core
 import { CSSProperties } from 'react';
 
 export function BorderStyle(border: Border | undefined, theme: Theme): CSSProperties {
-
-  if (!border) return { };
+  if (!border) return {};
 
   const borderTopColor = extractColor(theme, border, 'colorTop');
   const borderBottomColor = extractColor(theme, border, 'colorBottom');
   const borderLeftColor = extractColor(theme, border, 'colorLeft');
   const borderRightColor = extractColor(theme, border, 'colorRight');
-  const borderTopWidth = getValue(border, 'widthTop', 'widthAll');
-  const borderBottomWidth = getValue(border, 'widthBottom', 'widthAll');
-  const borderLeftWidth = getValue(border, 'widthLeft', 'widthAll');
-  const borderRightWidth = getValue(border, 'widthRight', 'widthAll');
+  const borderTopWidth = getValue(border, 'widthTop', 'width');
+  const borderBottomWidth = getValue(border, 'widthBottom', 'width');
+  const borderLeftWidth = getValue(border, 'widthLeft', 'width');
+  const borderRightWidth = getValue(border, 'widthRight', 'width');
 
   return {
     borderTopColor,
@@ -22,10 +21,10 @@ export function BorderStyle(border: Border | undefined, theme: Theme): CSSProper
     borderTopWidth,
     borderBottomWidth,
     borderLeftWidth,
-    borderRightWidth
-  }
+    borderRightWidth,
+  };
 }
 
 function extractColor(theme: Theme, border: Border, key: string): string {
-  return theme.colors[getValue(border, key, 'colorAll') as ColorToken];
+  return theme.colors[getValue(border, key, 'color') as ColorToken];
 }
