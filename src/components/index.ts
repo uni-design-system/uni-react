fad2e12c407916ac4165017dca53ca250d7bf7a9
George English <gaenglish@gmail.com>
2023-03-09 19:37:22 -0500

    adding indexes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,7 @@
 export * from './button';
 export * from './card';
+export * from './draggable';
+export * from './forms';
+export * from './icon-text-row';
+export * from './modal';
+export * from './switch';
