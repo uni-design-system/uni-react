6c1cee3a881b7babde4a05e4fde2c1430107152c
George English <gaenglish@gmail.com>
2023-04-01 12:38:07 -0400

    clean-up

--- src/utils/storage-manager.ts
+++ src/utils/storage-manager.ts
@@ -1,59 +1,48 @@
 export interface StorageManager<T> {
   type: 'cookie' | 'localStorage';
-  ssr?: boolean;
   get(init?: T): T | undefined;
   set(value: T): void;
 }
 
-export function createLocalStorageManager<T>(key: string): StorageManager<T> {
-  return {
-    ssr: false,
-    type: 'localStorage',
-    get(init?) {
-      if (!globalThis?.document) return init;
-      let value: any;
-      try {
-        value = localStorage.getItem(key) || init;
-      } catch (e) {
-        // no op
-      }
-
-      return JSON.parse(value) || init;
-    },
-    set(value) {
-      try {
-        localStorage.setItem(key, JSON.stringify(value));
-      } catch (e) {
-        // no op
-      }
-    },
-  };
-}
-
 // Usage
 // export const localStorageManager = createLocalStorageManager(STORAGE_KEY);
+export const createLocalStorageManager = <T>(key: string): StorageManager<T> => ({
+  type: 'localStorage',
+  get(init?) {
+    if (!globalThis?.document) return init;
+    let value: any;
+    try {
+      value = localStorage.getItem(key) || init;
+    } catch (e) {
+      // no op
+    }
 
-function parseCookie<T>(cookie: string, key: string): T | undefined {
+    return JSON.parse(value) || init;
+  },
+  set(value) {
+    try {
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (e) {
+      // no op
+    }
+  },
+});
+
+const parseCookie = (cookie: string, key: string): string | undefined => {
   const match = cookie.match(new RegExp(`(^| )${key}=([^;]+)`));
-  return match?.[2] as T | undefined;
-}
-
-export function createCookieStorageManager<T>(key: string, cookie?: string): StorageManager<T> {
-  return {
-    ssr: !!cookie,
-    type: 'cookie',
-    get(init?): T | undefined {
-      if (cookie) return parseCookie(cookie, key);
-      if (!globalThis?.document) return init;
-      return parseCookie(document.cookie, key) || init;
-    },
-    set(value) {
-      document.cookie = `${key}=${value}; max-age=31536000; path=/`;
-    },
-  };
-}
+  return match?.[2];
+};
 
 // Usage
 // export const cookieStorageManager = createCookieStorageManager(STORAGE_KEY);
-
-// export const cookieStorageManagerSSR = (cookie: string) => createCookieStorageManager(STORAGE_KEY, cookie);
+export const createCookieStorageManager = (key: string, cookie?: string): StorageManager<string> => ({
+  type: 'cookie',
+  get(init?): string | undefined {
+    if (cookie) return parseCookie(cookie, key);
+    if (!globalThis?.document) return init;
+    return parseCookie(document.cookie, key) || init;
+  },
+  set(value) {
+    document.cookie = `${key}=${value}; max-age=31536000; path=/`;
+  },
+});
