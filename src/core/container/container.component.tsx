3a7e7ecb24231136a0d7ad481962d9606d407f18
George English <gaenglish@gmail.com>
2023-04-07 12:05:53 -0400

    adding context to container

--- src/core/container/container.component.tsx
+++ src/core/container/container.component.tsx
@@ -1,15 +1,20 @@
-import React, {CSSProperties, ReactNode} from 'react';
+import React, { createContext, CSSProperties, ReactNode } from 'react';
+import { ContainerColorToken } from '@uni-design-system/uni-core';
 
 export interface ContainerProps {
   children: ReactNode;
   style: CSSProperties;
+  colorToken: ContainerColorToken;
 }
 
-export default function Container({ children, style, ...rest }: ContainerProps) {
+export const ContainerContext = createContext<{ colorToken?: ContainerColorToken }>({});
 
+export default function Container({ children, style, colorToken, ...rest }: ContainerProps) {
   return (
-    <div style={style} {...rest}>
-      {children}
-    </div>
-  )
+    <ContainerContext.Provider value={{ colorToken }}>
+      <div style={style} {...rest}>
+        {children}
+      </div>
+    </ContainerContext.Provider>
+  );
 }
